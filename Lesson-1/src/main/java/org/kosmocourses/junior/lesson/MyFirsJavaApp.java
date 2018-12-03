73384519f1f39d0953e79e3b9f45ebcf536e30b4
Vladislav Gasanov <kosmoflyko@gmail.com>
2018-12-03 08:54:15 +0300

    lesson-1

---
+++ Lesson-1/src/main/java/org/kosmocourses/junior/lesson/MyFirsJavaApp.java
@@ -0,0 +1,12 @@
+package org.kosmocourses.junior.lesson;
+
+public class MyFirsJavaApp {
+    public static String runProgramLogic(String name) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lesson-1/src/test/java/org/kosmocourses/junior/lesson/MyFirsJavaAppTest.java
@@ -0,0 +1,23 @@
+package org.kosmocourses.junior.lesson;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MyFirsJavaAppTest {
+    private static final String asset1_1 = "Я - Робот, вступая в ряды 'отряда космонавтов'\n"
+            + "Армии Java программистов, принимаю присягу и торжественно клянусь быть: \n"
+            + "\t- честным со своими товарищами бойцами\n"
+            + "\t- храбрым в борьбе с багами\n"
+            + "\t- дисциплинированным в сроках закрытия задач\n"
+            + "\t- строго хранить коммерческую тайну.\n"
+            + "\n"
+            + "Я клянусь добросовестно изучать IT, и всемерно беречь серверные мощности.\n"
+            + "Я всегда готов использовать свой разум на решение поставленных задач.\n"
+            + "Но несу моральную за код написанный мной.\n";
+
+    @Test
+    void runProgramLogicTest() {
+        assertEquals(asset1_1, MyFirsJavaApp.runProgramLogic("Робот"), "Сообщение выводимое программой не совпадает с тем что указано в условиях задачи");
+    }
+}
